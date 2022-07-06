ddfc824308549b7c258fe7703dab3f30d859f12b
kiviekelvin <kelvinkivie@gmail.com>
2022-07-06 13:47:28 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - fuction
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+	_putchar('\n');
+}

f0cae50dd7fee0a4a2287a7ad5ddc4095932e50d
kiviekelvin <kelvinkivie@gmail.com>
2022-07-07 16:53:55 -0700

    commiting this command

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - function
+ *
+ * @c: the character to print
+ *
+ * Return: int
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	} else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - function
+ *
+ * @a: the parameter
+ * @b: the parameter
+ *
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
